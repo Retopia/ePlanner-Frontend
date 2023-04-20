33d0d32ddb5ccefe244f6400301542ec0b8d2e46
Preston Tang <prestonleetang0123@gmail.com>
2023-04-20 10:12:56 -0400

    Installed vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import HomePage from './App';
 import reportWebVitals from './reportWebVitals';
+import { inject } from '@vercel/analytics';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+inject();
 
 // Other boilerplate code
 reportWebVitals();
